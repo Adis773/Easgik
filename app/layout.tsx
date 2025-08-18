761cd3a09f04f1b63a596b3e6c35d227421202f7
Adis773 <adis.abenov@mail.ru>
2025-08-18 09:18:29 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Easglik',
+  description: 'сайт с CRM',
+  generator: 'Easglik',
 }
 
 export default function RootLayout({
